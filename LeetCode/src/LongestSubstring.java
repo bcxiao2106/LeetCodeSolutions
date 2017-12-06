ff182d3b2649eae81d9242adf8e367e1c68c54ec
bcxiao2106 <bichengxiao16@gmail.com>
2017-12-06 17:43:31 -0500

    Update LongestSubstring

--- LeetCode/src/LongestSubstring.java
+++ LeetCode/src/LongestSubstring.java
@@ -1,85 +1,23 @@
+import java.util.Hashtable;
 
 public class LongestSubstring {
 	
-	public int lengthOfLongestSubstring(String s) {
-        if(!isRepeating(s)) return s.length();
-        for(int i = s.length() - 1; i > 1; i--){
-            String subStr = generateSubStrings(s, i);
-            if(subStr != null){
-	            System.out.println(subStr);
-	            return i;
-            }
-        }
-        return 1;
-    }
-	/*	
-	public int lengthOfLongestSubstring(String s) {
-        if(!isRepeating(s)) return s.length();
-        for(int i = s.length() - 1; i > 1; i--){
-            String[] subStrArr = generateSubStrings(s, i);
-            boolean isTarget = false;
-            for(int j = 0; j < subStrArr.length; j++){
-                if(!isRepeating(subStrArr[j])){
-                	isTarget = true;
-                	break;
-                }
-            }
-            System.out.println(isTarget);
-            if(isTarget) return i;
-        }
-        return 1;
-    }
-	
-    public String[] generateSubStrings(String s, int length){
-    	System.out.println(s + "  the length is: " + length);
-    	int combination  = s.length() - length + 1;
-    	System.out.println("Combination is : " + combination);
-        String[] resultString = new String[combination];
-        for(int i = 0; i < combination; i++){
-        	int startIdx = i;
-        	int endIdx = i + length - 1;
-        	if(endIdx < s.length()){
-        		resultString[i] = s.substring(startIdx, endIdx + 1);
-        		System.out.println(resultString[i]);
-        	}
-        }
-        return resultString;
-    }
-    */
-    
-    public String generateSubStrings(String s, int length){
-    	System.out.println(s + "  the length is: " + length);
-    	//int combination  = s.length() - length + 1;
-    	//System.out.println("Combination is : " + combination);
-        String resultString = "";
-        for(int i = 0; i < s.length() - length + 1; i++){
-        	int startIdx = i;
-        	int endIdx = i + length - 1;
-        	if(endIdx < s.length()){
-        		resultString = s.substring(startIdx, endIdx + 1);
-        		System.out.println(resultString);
-        		if(isRepeating(resultString)){
-        			i = i + length - 1;
-        		} else {
-        			return resultString;
-        		}
-        	}
-        }
-        return null;
-    }
-    
-    public boolean isRepeating(String s){
-        char[] charArr = s.toCharArray();
-        for(int i = 0; i < charArr.length; i++){
-            for(int j = i+1; j < charArr.length; j++){	
-                if(charArr[i] == charArr[j]){
-                	System.out.println(charArr[i] + "---" + charArr[j] + "  return true");
-                    return true;
-                }
-            }
-        }
-        System.out.println("This time returen false : " + s);
-        return false;
-    }
-
+	public static int lengthOfLongestSubstring(String s) {
+		Hashtable<Character,Integer> ht = new Hashtable<Character,Integer>();
+	    int n = s.length();
+	    int j = 1;
+	    int i = 0;
+	    int length = 0;
+	    ht.put(s.charAt(i),1);
+	    for(i = 0; i < n; i++){ 
+	        while(j < n && !ht.containsKey(s.charAt(j))){
+	        	ht.put(s.charAt(j), 1);
+	            j++;
+	        }
+	        int temp = j - i;
+	        length = temp > length ? temp : length;
+	        ht.remove(s.charAt(i));
+	    }
+	       return length;
+	}
 }
--- LeetCode/src/main.java
+++ LeetCode/src/main.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import interview.ClockAngle;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {-1,0,1,2,-1,-4};
-		ThreeSum ts = new ThreeSum(nums);
-		List<List<Integer>> resultLst = ts.findThreeSumEqualsToZero();
-
+		//int[] nums = {-1,0,1,2,-1,-4};
+		//ThreeSum ts = new ThreeSum(nums);
+		//List<List<Integer>> resultLst = ts.findThreeSumEqualsToZero();
+		//ClockAngle.findSmallerAngle(1, 5);
+		//ClockAngle.findEqualTimes();
+		String s = "pwwkew";
+		System.out.println(LongestSubstring.lengthOfLongestSubstring(s));
 	}
-
+	
+	
 }
